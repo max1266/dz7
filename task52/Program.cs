fb17605d8ed2bbd2c6db9a76742ac5ccb78790d4
Maxim_shmidt <history1266@mail.ru>
2022-12-20 18:22:59 +0700

    задача 52

---
+++ task52/Program.cs
@@ -0,0 +1,30 @@
+﻿void max(int[,] ma)
+{
+    for(int i = 0; i < ma.GetLength(0); i++)
+    {
+        for(int j = 0; j< ma.GetLength(1); j++)
+        {
+            ma[i,j] = new Random().Next(1,10);
+            Console.Write($"{ma[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+Console.Clear();
+Console.WriteLine("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+max(matrix);
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"Среднее арифмитическое: { sum / matrix.GetLength(0)} \t");
+}
